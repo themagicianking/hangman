d56a2d172ea90d8e3da85850ab3c2a40a6e57f81
themagicianking <tmoonder@gmail.com>
2023-06-01 21:05:23 -0400

    Created initially needed variables for initializing the Game class.

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,22 +1,16 @@
 module BoardGenerator
-  def create_static_board
-    puts "     ----------"
-    puts "    |          |"
-    puts "    O          |"
-    puts "   -|-         |"
-    puts "   / \\         |"
-    puts "               |"
-    puts "               |"
-    puts "  _ _ _ _ _ _ _"
-  end
-
-  def create_scoreboard
-  end
-
-  def create_used_letters
-  end
-
-  def create_guess
+  def create_board(score, word, letters, length)
+    puts "             ----------"
+    puts "            |          |"
+    puts "            #{score[1]}          |"
+    puts "           #{score[3]}#{score[2]}#{score[4]}         |"
+    puts "           #{score[5]} #{score[6]}         |"
+    puts "                       |"
+    puts "                       |"
+    puts " #{length[1]} #{length[2]} #{length[3]} #{length[4]} #{length[5]} #{length[6]} 
+    #{length[7]} #{length[8]} #{length[9]} #{length[10]} #{length[11]} #{length[12]}"
+    puts ""
+    puts "used letters: #{letters[1]}#{letters[2]}#{letters[3]}#{letters[4]}#{letters[5]}#{letters[6]}"
   end
 end
 
@@ -24,9 +18,20 @@ class Game
   include BoardGenerator
 
   def initialize
+    @score = {}
+    # get a word
+    @word = "fox"
+    @letters = {}
+    @length = {}
+    puts "Welcome to hangman! Your word to guess is #{@word.length} letters long."
   end
 end
 
-new_game = Game.new
+score = { 1 => "O", 2 => "|", 3 => "-", 4 => "-", 5 => "/", 6 => "\\" }
+word = "fox"
+letters = { 1 => "e" ", ", 2 => "g" ", ", 3 => "j" ", ", 4 => "k" ", ", 5 => "l" ", ", 6 => "m"}
+length = { 1 => " ", 2 => " ", 3 => " ", 4 => " ", 5 => " ", 6 => " ", 
+  7 => " ", 8 => " ", 9 => " ", 10 => " ", 11 => " ", 12 => " " }
 
-new_game.create_static_board
+new_game = Game.new
+new_game.create_board(score, word, letters, length)
